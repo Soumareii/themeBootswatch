9f451f5e24a3c7f0eec06395e889199f2bbc87e5
Soumareii <soumareii.is@gmail.com>
2021-06-28 10:43:56 +0000

    afficher announce

--- src/Controller/AnnounceController.php
+++ src/Controller/AnnounceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AnnounceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,22 @@ class AnnounceController extends AbstractController
     /**
      * @Route("/announce", name="announce")
      */
-    public function index(): Response
+    public function index(AnnounceRepository $repo): Response
     {
+        $announces = $repo->findAll();
         return $this->render('announce/index.html.twig', [
-            'controller_name' => 'AnnounceController',
+            'announces' => $announces,
+        ]);
+    }
+
+    /**
+     * @Route("/announce/{id}", name="announce_show")
+     */
+    public function show(AnnounceRepository $repo, int $id): Response
+    {
+        $announce = $repo->find($id);
+        return $this->render('announce/show.html.twig', [
+            'announce' => $announce,
         ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,30 +3,50 @@
 namespace App\DataFixtures;
 
 use App\Entity\Announce;
+use App\Entity\Comment;
+use App\Entity\Image;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for($i = 0; $i < 20; $i++){
+        $faker = Factory::create('fr_FR');
+        for($i = 0; $i < 30; $i++){
             $announce = new Announce();
-            $announce->setTitle("chambre n°$i");
-            $announce->setSlug("chambre-$i");
-            $announce->setDescription("Je vos loue ma chambre avec salle de bain neuve ... d");
+            $announce->setTitle($faker->sentence(3, false));
+            $announce->setIntroduction($faker->sentence());
+            $announce->setDescription($faker->text(600));
             $announce->setPrice(mt_rand(30000, 60000));
-            $announce->setAddress("Médina Rue 12");
-            $announce->setCoverImage("https://via.placeholder.com/500x300");
-            $announce->setRooms(mt_rand(0, 5));
+            $announce->setAddress($faker->address());
+            $announce->setCoverImage('https://picsum.photos/1300/600?random='.mt_rand(1,10000));
+            $announce->setRooms(mt_rand(1, 5));
             $announce->setIsAvailable(mt_rand(0, 1));
-            $announce->setCreatedAt(new DateTime());
+            $announce->setCreatedAt($faker->dateTimeBetween('-3 month', 'now'));
+
+            for($j = 0; $j < mt_rand(0, 7); $j++) {
+                $comment = new Comment();
+                $comment->setAuthor($faker->name())
+                         ->setEmail($faker->email())
+                         ->setContent($faker->text(200))
+                         ->setCreatedAt($faker->dateTimeBetween('-3 week', 'now'))
+                         ->setAnnounce($announce);
+                $announce->addComment($comment);        
+            }
+
+            for($k = 0; $k < mt_rand(2, 7); $k++) {
+                $image = new Image();
+                $image->setImageUrl('https://picsum.photos/500/350?random='.mt_rand(1,50000))
+                         ->setDescription($faker->sentence(3, false))
+                         ->setAnnounce($announce);
+                $announce->addImage($image);        
+            }
     
             $manager->persist($announce);   // Permet à Doctrine d'enregistrer l'annonce dans la DB 
         }
-
         $manager->flush();  // Execute l'enregistrement des données persistées
-
     }
 }
